a7aa23f30a1533b79fe3363f7fbafd475e7347d4
soohyun <soohyun_kim2@tmax.co.kr>
2021-10-15 17:59:28 +0900

    photo component one

--- src/components/shared.js
+++ src/components/shared.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const BaseBox = styled.div`
   background-color: ${(props) => props.theme.bgColor};
@@ -9,4 +9,8 @@ export const BaseBox = styled.div`
 export const FatLink = styled.span`
   font-weight: 600;
   color: rgb(142, 142, 142);
+`;
+
+export const FatText = styled.span`
+  font-weight: 600;
 `;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,59 @@
-import { useHistory } from "react-router";
-import { logUserOut } from "../apollo";
+import { useHistory } from 'react-router';
+import { logUserOut } from '../apollo';
+import { useReactiveVar, gql, useQuery } from '@apollo/client';
+import styled from 'styled-components';
+import Avatar from '../components/Avatar';
+import { FatText } from '../components/shared';
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      id
+      user {
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      comments
+      createdAt
+      isMine
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${(porps) => porps.theme.borderColor};
+  margin-bottom: 20px;
+`;
+const PhotoHeader = styled.div`
+  padding: 5px 10px;
+  display: flex;
+  align-items: center;
+`;
+
+const Username = styled(FatText)`
+  margin-left: 5px;
+`;
+
+const PhotoFooter = styled.div``;
 
 const Home = () => {
+  const { data } = useQuery(FEED_QUERY);
+  console.log(data);
   const history = useHistory();
   return (
     <div>
-      <h1>Home</h1>
-      <button onClick={() => logUserOut(history)}>Log out now!</button>
+      {data?.seeFeed?.map((photo) => (
+        <PhotoContainer key={photo?.id}>
+          <PhotoHeader>
+            <Avatar url={photo?.user?.avata} />
+            <Username>{photo?.user?.username}</Username>
+          </PhotoHeader>
+        </PhotoContainer>
+      ))}
     </div>
   );
 };
