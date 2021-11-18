525a7f75fa522f3af67d7bd543177ff7c51d1e5f
soohyun <soohyun_kim2@tmax.co.kr>
2021-11-18 16:29:57 +0900

    production api url

--- src/apollo.js
+++ src/apollo.js
@@ -31,7 +31,7 @@ export const disableDarkMode = () => {
 };
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: process.env.NODE_ENV === "production" ? 'https://soostaclone-backend.herokuapp.com/graphql' 'http://localhost:4000/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
